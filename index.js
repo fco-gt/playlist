f7c084b4ff2d4ec62d3ee2a34a6dd0138270fb6b
fco-gt <franomg10@gmail.com>
2024-01-21 22:52:13 -0300

    Respondive Media Query

---
+++ index.js
@@ -0,0 +1,14 @@
+// Github
+document.getElementById("git").addEventListener("click", function (event) {
+    window.open('https://github.com/fco-gt');
+});
+
+// Instagram
+document.getElementById("ig").addEventListener("click", function (event) {
+    window.open('https://www.instagram.com/fco_gt.wav/');
+});
+
+// Discord
+document.getElementById("ds").addEventListener("click", function (event) {
+    window.open('https://top.gg/bot/758059320238931978');
+});
